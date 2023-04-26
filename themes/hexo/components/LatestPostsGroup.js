8fa11490938e8bbbcd67dbc2d226b69aa305d1de
anjhon <44490800+An-Jhon@users.noreply.github.com>
2023-04-26 11:57:17 +0800

    Update LatestPostsGroup.js

--- themes/hexo/components/LatestPostsGroup.js
+++ themes/hexo/components/LatestPostsGroup.js
@@ -40,7 +40,7 @@ const LatestPostsGroup = ({ latestPosts, siteInfo }) => {
           className={'my-2 flex'}>
 
           <div
-            className="w-20 h-15 bg-none bg-center bg-no-repeat"
+            className="w-20 h-14 bg-cover bg-center bg-no-repeat"
             style={{ backgroundImage: headerImage }}
           />
           <div
